4d8c07a34e8c5438d1c3330d16e0fba69ea5ba73
DaMiRkIn29 <142334840+DaMiRkIn29@users.noreply.github.com>
2024-07-05 22:14:22 +0300

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,8 +0,0 @@
-print("1st Program")
-print(9 ** 0.5 * 5)
-print("2nd program")
-print(9.99>9.98 and 1000 != 1000.1)
-print("3rd Program")
-print((1234 // 10 % 100) + (5678 // 10 % 100))
-print("4th Program")
-print((int(13.42)) == (42.13 * 100 % 100) or (26.15 * 100 % 100) == (int(15.26)))

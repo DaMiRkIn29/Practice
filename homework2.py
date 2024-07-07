9946d90ff58c48b127140d1cd773b49c9b4190db
DaMiRkIn29 <142334840+DaMiRkIn29@users.noreply.github.com>
2024-07-07 20:48:25 +0300

    Create homework2.py

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework = 12
+hours_of_work = 1.5
+course_name = 'Python'
+one_task_time = hours_of_work / homework
+print('Курс:', course_name, ' Всего задач:', homework, ' Затрачено часов:', hours_of_work, ' Среднее время выполнения', one_task_time, 'часа.')

6a3955e050d5d1bc1cd581c730f51f01526e859d
DaMiRkIn29 <142334840+DaMiRkIn29@users.noreply.github.com>
2024-07-16 22:34:55 +0300

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -8,7 +8,7 @@ result = ""
 
 for i in range(1, first_num):
     for j in range(i, first_num):
-        if first_num % (i + j) == 0 and i and j != 1 and 1:
+        if first_num % (i + j) == 0 and i != j:
             couples.append([i, j])
             result += str(i) + str(j)
             
